de7928943d9f4b3c4f8e869b5297d3159ae6ee5d
Paulinemkavita <114083982+Paulinemkavita@users.noreply.github.com>
2022-10-21 15:28:38 +0300

    index.html with js commits

--- calc.js
+++ calc.js
@@ -1,25 +1,22 @@
-const additionFunction = (inputOne, inputTwo) => {
-    return inputOne + inputTwo
-}
-const subtractionFunction = (inputOne, inputTwo) => {
-    return inputOne - inputTwo
-}
-const multiplicationFunction = (inputOne, inputTwo) => {
-    return inputOne * inputTwo
-}
-const divisionFunction = (inputOne, inputTwo) => {
-    return inputOne / inputTwo
-}
-const myCalculatorProgram =() => {
-    //ask user for inputOne
-    const numberOne = prompt('please state the first number')
-    //ask user for inputTwo
-    const numberTwo = prompt('please state the second number')
-    //ask user for operation they want to be done
-    const operation = prompt('please state the type of operation')
-    //perform the operation basedd on the type specified above
-    if (operation == '+') {
-        additionFunction(numberOne, numberTwo)
-    }
+const submit= () => {
+//retrieve elements
+const dateElement = document.getElementById("date")
+const genderElement = document.getElementById("gender")
+// const maleElement = document.getElementById("male")
+
+//retrieving input values
+
+const date = dateElement.value;
+const gender = genderElement.value;
+//const male = maleElement.value
+
+
+const submit = [date,gender]
+
+console.log(submit)
+
+
+
+
 
 }
